738d43970573ff5dc9942d29a3b3dbb7a21f21cf
Ana Córcoles <ana.corcoles@gmail.com>
2022-02-02 18:10:33 +0100

    Add files via upload

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var numero int
+var texto string
+var booleano bool
+
+
+func main()  {
+	numero2, numero3, texto2 :2, 3, "texto"
+	fmt.Println(numero2)
+	fmt.Println(numero3)
+	fmt.Println(texto2)
+}
